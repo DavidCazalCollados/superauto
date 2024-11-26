829f5f83f7c72ad7a793a66d8f7c37d8ea6ca49b
David Cazal <da.cazal95@gmail.com>
2024-11-26 14:05:35 -0500

    Displayed the cars in the index page

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,4 +3,8 @@ class PagesController < ApplicationController
 
   def home
   end
+
+  def dashboard
+    @cars = current_user.cars
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -8,4 +8,5 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "posts#index"
   resources :cars, only: [:index, :show, :new, :create]
+  get "dashboard", to: "pages#dashboard"
 end
