56ca48bafdf24548e205843e1cd5a08d7420c219
David Cazal <da.cazal95@gmail.com>
2024-11-27 15:51:03 -0500

    fixed issues with migrations and seeds

--- db/migrate/20241127174813_create_bookings.rb
+++ db/migrate/20241127174813_create_bookings.rb
@@ -1,8 +1,8 @@
 class CreateBookings < ActiveRecord::Migration[7.1]
   def change
     create_table :bookings do |t|
-      t.string :start_date, presence: true
-      t.string :end_date, presence: true
+      t.string :start_date, null: false
+      t.string :end_date, null: false
       t.boolean :accepted, default: nil
       t.references :user, null: false, foreign_key: true
       t.references :car, null: false, foreign_key: true
