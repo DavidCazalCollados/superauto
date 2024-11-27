2b99362193fa0e40c680eca9679200077111bd5e
David Cazal <da.cazal95@gmail.com>
2024-11-27 15:16:32 -0500

    YOu can create a booking

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,5 +1,28 @@
 class BookingsController < ApplicationController
-  def index
-    @bookings = current_user.bookings.all
+  before_action :set_car, only: [:new, :create]
+
+  def new
+    @booking = Booking.new
+  end
+
+  def create
+    @booking = Booking.new(booking_params)
+    @booking.car = @car
+    @booking.user = current_user
+    if @booking.save
+      redirect_to dashboard_path, notice: "Booking created succesfully"
+    else
+      render :new, status: :unprocessable_entity, notice: "something went wrong"
+    end
+  end
+
+  private
+
+  def booking_params
+    params.require(:booking).permit(:start_date, :end_date)
+  end
+
+  def set_car
+    @car = Car.find(params[:car_id])
   end
 end
