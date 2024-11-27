566a4ea41fa3540b5de9260a9f49e806ec95a218
David Cazal <da.cazal95@gmail.com>
2024-11-27 12:11:27 -0500

    fake cars added to seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,9 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+Car.create!(title: "Hyundai Elantra", description: "Great Car!", price_per_day: 200, user_id: 2)
+Car.create!(title: "Chevrolet", description: "Yeahhhh", price_per_day: 800, user_id: 2)
+Car.create!(title: "Ferrari", description: "Sport car", price_per_day: 1000, user_id: 2)
+Car.create!(title: "Tesla", description: "Electric car", price_per_day: 900, user_id: 2)
+Car.create!(title: "Peugeot", description: "French car", price_per_day: 500, user_id: 2)
