716418018b9082a363127063cbb96f3244c0666b
David Cazal <da.cazal95@gmail.com>
2024-11-28 17:53:09 -0500

    almost there...

--- app/javascript/controllers/total_price_controller.js
+++ app/javascript/controllers/total_price_controller.js
@@ -2,13 +2,29 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="total-price"
 export default class extends Controller {
-  static targets = ["pricePerDay"]
+  static targets = ["pricePerDay", "startDate", "endDate"]
 
   connect() {
     console.log("Hello from our first Stimulus controller");
     console.log(this.pricePerDayTarget.innerText)
+
   }
 
-  price() {
+  price(event) {
+    console.log(event);
+    const startDate = Date.parse(this.startDateTarget.value);
+    const endDate = Date.parse(this.endDateTarget.value);
+
+    const daysConvert = (1000 * 24 * 60 * 60);
+
+    const diffTime = ((endDate) - (startDate)) / daysConvert;
+
+    const intDateDays = (parseInt(this.pricePerDayTarget.innerText, 10));
+
+    if (endDate >= startDate) {
+      const intDate = intDateDays * diffTime
+
+      this.pricePerDayTarget.innerText = `Total: ${intDate}$`
+    }
   }
 }
